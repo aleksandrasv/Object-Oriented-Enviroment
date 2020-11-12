84c3ef7c399a0a8503ce3841591d805e365fe2e1
Soroush <skaram5@uic.edu>
2020-11-11 18:54:14 -0800

    Initial commit

---
+++ src/main/java/edu/uic/cs474/a2/Main.java
@@ -0,0 +1,12 @@
+package edu.uic.cs474.a2;
+
+
+public class Main {
+
+    private Main() { /* Empty */ }
+
+    static ReflectionUtils getReflectionUtils() {
+        throw new Error("Not implemented");
+    }
+
+}
